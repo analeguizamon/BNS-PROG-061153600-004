01f88e140c099b750236b2a2b0f4256cf3012fe7
anA <129904087+analeguizamon@users.noreply.github.com>
2023-09-29 09:25:08 -0300

    Delete bns-prog-061153600-004 directory

--- bns-prog-061153600-004/js/loginScript.js
+++
@@ -1,7 +0,0 @@
-const claveInput = document.getElementById("clave");
-const claveCheckbox = document.getElementById("censurarClave");
-
-claveCheckbox.addEventListener("change", function () {
-    if (claveCheckbox.checked) claveInput.type = "text";
-    else claveInput.type = "password";
-});
